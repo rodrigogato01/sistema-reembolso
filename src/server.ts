bd98f380cf0a92409a7b7c9210a79a94ebb60dfd
Rodrigo <rodrigogato041@gmail.com>
2026-02-08 03:30:58 -0300

    Adicionando CORS para liberar acesso

--- src/server.ts
+++ src/server.ts
@@ -1,25 +1,14 @@
-import express from 'express'; // <--- Faltava importar
-import cors from 'cors';       // <--- Faltava importar
-import path from 'path';       // <--- Faltava importar
-import 'dotenv/config';        // <--- Carrega o arquivo .env para todo o projeto
-import { router } from './routes'; // <--- Faltava importar suas rotas
+import express from 'express';
+import cors from 'cors'; // Importa a permissão
+import { router } from './routes';
 
 const app = express();
 
-// Configurações do Servidor
-app.use(cors());
+// AQUI ESTÁ O SEGREDO: Libera o acesso para qualquer site (incluindo o seu teste local)
+app.use(cors()); 
+
 app.use(express.json());
-
-// Configura a pasta pública (onde fica o site bonito)
-app.use(express.static(path.join(__dirname, '../public')));
-
-// Usa as rotas que criamos (PixController, etc)
 app.use(router);
 
-// Configuração de Porta (Importante para o Deploy na Render)
-// Se estiver no PC usa 3000, se estiver na Render usa a porta que eles mandarem
 const PORT = process.env.PORT || 3000;
-
-app.listen(PORT, () => {
-    console.log(`🚀 Sistema rodando na porta ${PORT}`);
-});
+app.listen(PORT, () => console.log(`Servidor rodando na porta ${PORT}`));
