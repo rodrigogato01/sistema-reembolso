4d6fbdbfdb9fb71e305e832e98987db31947b03e
Rodrigo <rodrigogato041@gmail.com>
2026-02-08 03:37:10 -0300

    Instalando CORS para liberar acesso

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,10 @@
 import express from 'express';
-import cors from 'cors'; // Importa a permissão
+import cors from 'cors'; // <--- O porteiro chegou
 import { router } from './routes';
 
 const app = express();
 
-// AQUI ESTÁ O SEGREDO: Libera o acesso para qualquer site (incluindo o seu teste local)
+// AQUI ESTÁ O SEGREDO: Libera o acesso para qualquer site (incluindo o seu teste)
 app.use(cors()); 
 
 app.use(express.json());
