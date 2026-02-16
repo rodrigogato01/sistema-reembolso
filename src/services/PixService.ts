6c41a718b7bde6b538520b04b7fc54fc2cc0bdd8
rodrigogato01 <rodrigogato041@gmail.com>
2026-02-16 16:43:59 -0300

    Update PixService.ts

--- src/services/PixService.ts
+++ src/services/PixService.ts
@@ -1,10 +1,3 @@
-import { MercadoPagoConfig, Payment, PaymentRefund } from 'mercadopago';
-
-const client = new MercadoPagoConfig({
-    accessToken: process.env.MP_ACCESS_TOKEN || '', 
-    options: { timeout: 5000 }
-});
-
 const payment = new Payment(client);
 const refundClient = new PaymentRefund(client);
 
